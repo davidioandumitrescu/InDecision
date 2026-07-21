7a9bdff706d464d3f820aca128ea6f1613b8ee7c
David-Ioan Dumitrescu <davidioan.dumitrescu@gmail.com>
2026-07-21 12:52:34 +0800

    added edit and delete fucntionality if you are the host

---
+++ InDecision/ExperienceEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ExperienceEditView.swift
+//  InDecision
+//
+//  Created by David-Ioan Dumitrescu on 21/7/2026.
+//
+
+import SwiftUI
+
+struct ExperienceEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ExperienceEditView()
+}
